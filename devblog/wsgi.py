1c0edca246f89794044cfad05e91f91a4b5b6d02
Mridul Banik <banikmridul112@gmail.com>
2018-06-06 12:31:24 +0600

    Update wsgi.py

--- devblog/wsgi.py
+++ devblog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qblog project.
+WSGI config for devblog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qblog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devblog.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
